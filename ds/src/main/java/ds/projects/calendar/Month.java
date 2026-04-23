c4d51b45874499b923c000e202df845d858c7b0f
adirstrauchler <adirstrauchler@gmail.com>
2026-04-23 18:03:19 -0400

    switch statment

--- ds/src/main/java/ds/projects/calendar/Month.java
+++ ds/src/main/java/ds/projects/calendar/Month.java
@@ -6,23 +6,57 @@ public class Month{
 
 	protected Months monthName;
 
-	protected enum Months {JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER, TISHREI, CHESHVAN, KISLEV, SHVAT, ADDAR_ALEF, ADDAR_BET, NISSAN, IYAR, SIVAN, TAMUZ, AV, ELLUL}
+	private Day[] days;
+
+	protected enum Months {JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER, TISHREI, CHESHVAN, KISLEV, TEVET, SHVAT, ADDAR_ALEF, ADDAR_BET, NISSAN, IYAR, SIVAN, TAMUZ, AV, ELLUL}
 
 	//positve if english negative if hebrew
-	public Month(int Length, int month){
-		
+	public Month(int length, int month){
+		this.days = new Day[lenght];
+		this.monthName = switch (month) {
+			case 1 -> Months.JANUARY;
+			case 2 -> Months.FEBRUARY;
+			case 3 -> Months.MARCH;
+			case 4 -> Months.APRIL;
+			case 5 -> Months.MAY;
+			case 6 -> Months.JUNE;
+			case 7 -> Months.JULY;
+			case 8 -> Months.AUGUST;
+			case 9 -> Months.SEPTEMBER;
+			case 10 -> Months.OCTOBER;
+			case 11 -> Months.NOVEMBER;
+			case 12 -> Months.DECEMBER;
+
+			case -1 -> Months.TISHREI;
+			case -2 -> Months.CHESHVAN;
+			case -3 -> Months.KISLEV;
+			case -4 -> Months.TEVET;
+			case -5 -> Months.SHVAT;
+			case -6 -> Months.ADDAR_ALEF;
+			case -7 -> Months.ADDAR_BET;
+			case -8 -> Months.NISSAN;
+			case -9 -> Months.IYAR;
+			case -10 -> Months.SIVAN;
+			case -11 -> Months.TAMUZ;
+			case -12 -> Months.AV;
+			case -13 -> Months.ELLUL;
+		}
+
 	}
 
-	private void addDay(Day day) {
-
+	public void addDay(Day day, int number) {
+		if(number > this.days.length){
+			throw new IllegalArgumentException("Month does not contain " + number + " days.");
+		}
+		this.days[number] = month;
 	}
 
 	public Day[] getDays(){
-		return null;
+		return this.days;
 	}
 
 	public int lenght() {
-
+		return this.days.lenght;
 	}
 
 }
